bb1f1ef88daeaa5be5ed1d212b0103e041d7399b
berna39 <kalemajoseph39@gmail.com>
2023-05-14 03:14:24 +0200

    nested tests

--- src/test/java/com/terminator/MultipleTest.java
+++ src/test/java/com/terminator/MultipleTest.java
@@ -3,6 +3,7 @@ package com.terminator;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class MultipleTest {
@@ -17,5 +18,38 @@ public class MultipleTest {
                 () -> assertEquals(25, a * a),
                 () -> assertEquals(1, a / a)
         ); // if one test fails, all the test will fail
+    }
+
+    @Nested
+    class GroupTest{
+        // if one test fails, all the test will fail
+
+        @Test
+        @DisplayName("Testing addition")
+        void additionTest(){
+            int a = 5;
+            assertEquals(10, a + a);
+        }
+
+        @Test
+        @DisplayName("Testing sub")
+        void subTest(){
+            int a = 5;
+            assertEquals(0, a - a);
+        }
+
+        @Test
+        @DisplayName("Testing multiply")
+        void multiplyTest(){
+            int a = 5;
+            assertEquals(25, a * a);
+        }
+
+        @Test
+        @DisplayName("Testing divide")
+        void divideTest(){
+            int a = 5;
+            assertEquals(1, a / a);
+        }
     }
 }
