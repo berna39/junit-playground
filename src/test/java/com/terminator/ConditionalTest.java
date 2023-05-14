3172d756c61987901e26dfa93dec35826d3239ed
berna39 <kalemajoseph39@gmail.com>
2023-05-14 02:38:45 +0200

    assumption before testing

--- src/test/java/com/terminator/ConditionalTest.java
+++ src/test/java/com/terminator/ConditionalTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 public class ConditionalTest {
 
@@ -14,5 +15,13 @@ public class ConditionalTest {
     @DisplayName("OS Conditional test")
     void specificOS(){
         assertEquals(2, 2);
+    }
+
+
+    @Test
+    void assumption(){
+        Boolean imCool = true;
+        assumeTrue(imCool); //  if this is false the test will be skipped
+        assertEquals("Toby", "Toby");
     }
 }
